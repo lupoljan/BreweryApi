0917bfc216956cf913a3f5a73d6933b3f74ee9ae
Łukasz P <lupoljan@o2.pl>
2025-08-17 12:50:17 +0200

    controller refactor, add logging

--- Controllers/BreweriesController.cs
+++ Controllers/BreweriesController.cs
@@ -1,4 +1,5 @@
-﻿using BreweryApi.Services;
+﻿using BreweryApi.Models;
+using BreweryApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BreweryApi.Controllers
@@ -8,28 +9,38 @@ namespace BreweryApi.Controllers
     public class BreweriesController : ControllerBase
     {
         private readonly IBreweryService _breweryService;
+        private readonly ILogger<BreweriesController> _logger;
 
-        public BreweriesController(IBreweryService breweryService)
+        public BreweriesController(IBreweryService breweryService, ILogger<BreweriesController> logger)
         {
             _breweryService = breweryService;
+            _logger = logger;
         }
 
+        /// <summary>
+        /// Get a list of breweries with optional search, sorting, and distance calculation.
+        /// </summary>
+        /// <param name="search">Search term for brewery name or city</param>
+        /// <param name="sort">Sort by "name", "city", or "distance"</param>
+        /// <param name="latitude">Latitude for distance sorting</param>
+        /// <param name="longitude">Longitude for distance sorting</param>
+        /// <returns>List of breweries</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<BreweryDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetBreweries(
             [FromQuery] string? search,
             [FromQuery] string? sort,
             [FromQuery] double? latitude,
             [FromQuery] double? longitude)
         {
-            try
-            {
-                var breweries = await _breweryService.GetBreweriesAsync(search, sort, latitude, longitude);
-                return Ok(breweries);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { message = "An error occurred.", detail = ex.Message });
-            }
+            _logger.LogInformation("Fetching breweries. Search={search}, Sort={sort}, Lat={latitude}, Lon={longitude}",
+                search, sort, latitude, longitude);
+
+            var breweries = await _breweryService.GetBreweriesAsync(search, sort, latitude, longitude);
+
+            return Ok(breweries);
         }
     }
 }
