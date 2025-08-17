6d2585e2633b9e475e48bf0d6072acf4ac780c1f
Łukasz P <lupoljan@o2.pl>
2025-08-17 14:28:44 +0200

    fix mappings

--- Models/OpenBreweryResponse.cs
+++ Models/OpenBreweryResponse.cs
@@ -6,19 +6,18 @@
         public string id { get; set; } = string.Empty;
         public string name { get; set; } = string.Empty;
         public string brewery_type { get; set; } = string.Empty;
-        public string street { get; set; } = string.Empty;
+        public string address_1 { get; set; } = string.Empty;
         public string address_2 { get; set; } = string.Empty;
         public string address_3 { get; set; } = string.Empty;
         public string city { get; set; } = string.Empty;
-        public string state { get; set; } = string.Empty;
-        public string county_province { get; set; } = string.Empty;
+        public string state_province { get; set; } = string.Empty;
         public string postal_code { get; set; } = string.Empty;
         public string country { get; set; } = string.Empty;
-        public string longitude { get; set; } = string.Empty;
-        public string latitude { get; set; } = string.Empty;
+        public double? longitude { get; set; }
+        public double? latitude { get; set; } 
         public string phone { get; set; } = string.Empty;
         public string website_url { get; set; } = string.Empty;
-        public DateTime? updated_at { get; set; }
-        public DateTime? created_at { get; set; }
+        public string state { get; set; } = string.Empty;
+        public string street { get; set; } = string.Empty;
     }
 }
--- Utilities/MappingProfile.cs
+++ Utilities/MappingProfile.cs
@@ -11,9 +11,14 @@ namespace BreweryApi.Utilities
             // External API → Domain
             CreateMap<OpenBreweryResponse, BreweryModel>()
                 .ForMember(dest => dest.Longitude,
-                    opt => opt.MapFrom(src => ParseNullableDouble(src.longitude)))
-                .ForMember(dest => dest.Latitude,
-                    opt => opt.MapFrom(src => ParseNullableDouble(src.latitude)))
+                    opt => opt.MapFrom(src => src.longitude))
+                 .ForMember(dest => dest.Latitude,
+                    opt => opt.MapFrom(src => src.latitude))
+                .ForMember(dest => dest.BreweryType, opt => opt.MapFrom(src => src.brewery_type))
+                .ForMember(dest => dest.Address1, opt => opt.MapFrom(src => src.address_1))
+                .ForMember(dest => dest.Address2, opt => opt.MapFrom(src => src.address_2))
+                .ForMember(dest => dest.Address3, opt => opt.MapFrom(src => src.address_3))
+                .ForMember(dest => dest.StateProvince, opt => opt.MapFrom(src => src.state_province))
                 .ForMember(dest => dest.PostalCode, opt => opt.MapFrom(src => src.postal_code))
                 .ForMember(dest => dest.Website, opt => opt.MapFrom(src => src.website_url));
 
